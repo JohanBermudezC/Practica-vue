b5c3b176f0d96754827184e04129133567596519
JohanBermudezC <jsbermudezc@ufpso.edu.co>
2024-05-17 17:42:01 -0500

    Practica-vue

---
+++ src/components/FAQ.vue
@@ -0,0 +1,41 @@
+
+<script setup>
+
+import { ref } from 'vue'
+
+const data = ['Tema 1', 'Tema 2', 'Tema 3', 'Tema 4']
+const selectedIndex = ref(0)
+
+function showInfo(i) {
+    if (i == selectedIndex.value) {
+        selectedIndex.value = -1
+    } else{
+        selectedIndex.value = i
+    }
+}
+</script>
+
+<template>
+    <div class="bg-blue-500 p-1 max-w-60 mx-auto mt-12  ">
+
+        <div v-for="(theme, i) in data"
+            :key="i"
+        >
+            <h1 
+            @click="showInfo(i)"
+            class= "hover:cursor-pointer bg-blue-400 text-white px-1 flex justify-between select-none  "
+            :class="{'bg-blue-700': selectedIndex == i}"
+            >
+                {{ theme }}
+                <span>{{ selectedIndex == i ? '-' : '+' }}</span>
+            </h1>
+            <div 
+            v-show="selectedIndex == i"
+            class="bg-blue-50/50 p-1 mt-1 select-none h-20">
+                Texto del tema 
+            </div>
+        </div>
+
+        
+    </div>
+</template>
